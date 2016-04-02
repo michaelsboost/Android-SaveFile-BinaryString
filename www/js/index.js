b02303ee574d674c5430715754e3534ae56ea17f
Michael Schwartz <mikethedj4@yahoo.com>
2016-04-02 18:06:56 -0500

    Save file code added for experimentation

--- www/js/index.js
+++ www/js/index.js
@@ -1,14 +1,8 @@
-var txt = document.querySelector("[data-action=input-list]")
-
-if ( localStorage.getItem("alphabetize")) {
-  txt.value = localStorage.getItem("alphabetize");
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+  // Device is ready
 }
 
-document.querySelector("[data-action=alphabetize]").onclick = function() {
-  txt.value = (txt.value.split("\n").sort(caseInsensitive).join("\n"))
-
-  function caseInsensitive(a, b) {
-    return a.toLowerCase().localeCompare(b.toLowerCase())
-  }
-  localStorage.setItem("alphabetize", txt.value);
-}
+$('.download').click(function() {
+  setTimeout($('#jszipdemo').val())
+})
