81462adb8f712a33280234b6c7ea40f849ae47ac
Michael Schwartz <mikethedj4@yahoo.com>
2016-04-03 10:41:31 -0500

    removed jszip

--- www/js/index.js
+++ www/js/index.js
@@ -3,30 +3,6 @@ function onDeviceReady() {
   // Device is ready
 }
 
-function saveFile(fileName, fileData) {
-  // Get access to the file system
-  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
-    // Create the file.
-    fileSystem.root.getFile(fileName, { create: true, exclusive: false }, function (entry) {
-      // After you save the file, you can access it with this URL
-      var myFileUrl = entry.toURL()
-      entry.createWriter(function(writer) {
-        writer.onwriteend = function(evt) {
-          alert("Successfully saved file to " + myFileUrl)
-        }
-        // Write to the file
-        writer.write(fileData)
-      }, function(error) {
-        alert("Error: Could not create file writer, " + error.code)
-      })
-    }, function(error) {
-      alert("Error: Could not create file, " + error.code)
-    })
-  }, function(evt) {
-    alert("Error: Could not access file system, " + evt.target.error.code)
-  })
-}
-
-$(".download").click(function() {
-  saveFile("hello.html", "hello world")
+$("[data-action=download]").click(function() {
+  setTimeout($("[data-action=runstring]").val())
 })
